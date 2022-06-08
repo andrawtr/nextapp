fa33c20ec318c313a86e9f437a25f40d044bb300
andrawtr <andrawitantra.aw@gmail.com>
2022-06-09 00:24:49 +0700

    cek upload ke firebase app distribution

---
+++ app/src/main/java/com/skripsi/andra/buatevent.java
@@ -0,0 +1,14 @@
+package com.skripsi.andra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class buatevent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buatevent);
+    }
+}
