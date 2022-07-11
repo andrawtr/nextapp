c9b3b8c533784bcff4ee05f93656cf95e5709a71
andrawtr <idriskdmundu@gmail.com>
2022-07-12 05:32:48 +0700

    unit testing added

---
+++ app/src/test/java/com/skripsi/andra/cekvalidTest.java
@@ -0,0 +1,32 @@
+package com.skripsi.andra;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class cekvalidTest {
+
+    @Test
+    public void valemailWhenEmailIsEmptyThenReturnEmailWajibDiisi() {
+        String email = "";
+        String expected = "Email wajib diisi";
+        String actual = cekvalid.valemail(email);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void valemailWhenEmailIsNotValidThenReturnAlamatEmailTidakValid() {
+        String email = "john@gmail";
+        String expected = "Alamat Email tidak valid";
+        String actual = cekvalid.valemail(email);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void valemailWhenEmailIsValidThenReturnAlamatEmailTidakValid() {
+        String email = "john@gmail.com";
+        String expected = "true";
+        String actual = cekvalid.valemail(email);
+
+        assertEquals(expected, actual);
+    }
+}
